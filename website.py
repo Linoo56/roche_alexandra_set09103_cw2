b5db486fcc4d2ad3d9f98faa0ac827d186ba85bd
Alexandra Roche <40404291@live.napier.ac.uk>
2018-10-15 09:40:07 +0100

    import database

--- website.py
+++ website.py
@@ -9,27 +9,26 @@ def root():
 @app.route('/config/')
 def config():
 	str=[]
-	str.append('Debug:'+app.config['DEBUG'])
 	str.append('port:'+app.config['port'])
 	str.append('url:'+app.config['url'])
-	str. append ('ip_address :'+app. config ['ip_address '])
-	return '\t'. join (str )
+	str.append('ip_address :'+app.config['ip_address'])
+	return '\t'.join(str)
 
 def init (app):
-	config = ConfigParser . ConfigParser ()
+	config = ConfigParser.ConfigParser()
 	try:
-		config_location = "etc/ defaults .cfg"
-		config . read ( config_location )
+		config_location = "etc/defaults.cfg"
+		config.read(config_location)
 
-		app. config ['DEBUG '] = config .get(" config ", " debug ")
-		app. config ['ip_address '] = config .get(" config ", " ip_address")
-		app. config ['port '] = config .get(" config ", " port ")
-		app. config ['url '] = config .get(" config ", "url")
+		app.config['DEBUG'] = config.get("config", "debug")
+		app.config['ip_address'] = config.get("config", "ip_address")
+		app.config['port'] = config.get("config", "port")
+		app.config['url'] = config.get("config", "url")
 	except :
 		print " Could not read configs from : ", config_location
 
-if __name__ == '__main__ ':
-	init ( app)
+if __name__ == '__main__':
+	init(app)
 	app.run(
-	host =app . config ['ip_address '],
-	port =int (app . config ['port ']))
+	host =app.config['ip_address'],
+	port =int(app.config ['port']))
