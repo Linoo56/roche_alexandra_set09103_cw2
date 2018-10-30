385b8fe24ccf6eae173ad68367c2c38fca1d0d85
Alexandra Roche <40404291@live.napier.ac.uk>
2018-10-30 13:47:22 +0000

    Database Initialization

---
+++ program.py
@@ -0,0 +1,15 @@
+class Program:
+	
+	def __init__(self, proId, schId, duration, appliFee, courseFee, accoType, accoFee, visaSupp, eju, jlpt, notes):
+		self.proId = proId
+		self.schId = schId
+		self.duartion = duration
+		self.appliFee = appliFee
+		self.courseFee = courseFee
+		self.accoType = accoType
+		self.accofee = accoFee
+		self.visaSupp = visaSupp
+		self.eju = eju
+		self.jlpt = jlpt
+		self.notes = notes
+
